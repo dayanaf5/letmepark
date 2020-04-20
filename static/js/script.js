930936d5643923d8c0ef25bce9a2dbcca1e31f65
J. Hernan Ramirez R <hr@hernanramirez.info>
2020-04-20 06:34:35 -0400

    Ajuste de mapa para busquedas por distancia. Se agrega la biblioteca leaft mas reciente

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 var mapa;
 var tipo;
 
+
 function cargar_mapa() {
     mapa = new L.Map('mapa', { zoomControl: true });
     var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -10,13 +11,26 @@ function cargar_mapa() {
 }
 
 
+function elegirDireccion(lat, lng, tipo_osm) {
 
+    var point = L.latLng(lat, lng);
+    
+    /*
+    500 representa metros, pueden cambiarlo a discrecion, incluso colocar
+    un popup para que el usuario pueda conltar a 500/800/1000
+    */
 
-function elegirDireccion(lat1, lng1, lat2, lng2, tipo_osm) {
-    var loc1 = new L.LatLng(lat1, lng1);
-    var loc2 = new L.LatLng(lat2, lng2);
-    var bounds = new L.LatLngBounds(loc1, loc2);
+    var bounds = point.toBounds(500); 
 
+    var l1 = bounds['_southWest'];
+    var l2 = bounds['_northEast'];
+
+    var lat1 = l1.lat;
+    var lat2 = l2.lat;
+    var lng1 = l1.lng;
+    var lng2 = l2.lng;
+        
+    
     if (tipo) {
         mapa.removeLayer(tipo);
     }
@@ -28,10 +42,11 @@ function elegirDireccion(lat1, lng1, lat2, lng2, tipo_osm) {
     else {
         var loc3 = new L.LatLng(lat1, lng2);
         var loc4 = new L.LatLng(lat2, lng1);
-        tipo = L.polyline([loc1, loc4, loc2, loc3, loc1], { color: 'grey' }).addTo(mapa);
+        tipo = L.polyline([l1, loc4, l2, loc3, l1], { color: 'grey' }).addTo(mapa);
         mapa.fitBounds(bounds);
     }
-    valor(loc1,loc2);
+    
+    valor(l1,l2);
 }
 
 
@@ -39,12 +54,15 @@ function elegirDireccion(lat1, lng1, lat2, lng2, tipo_osm) {
 function direccion_buscador() {
     var entrada = document.getElementById("direccion");
 
-    $.getJSON('http://nominatim.openstreetmap.org/search?format=json&limit=5&q=' + entrada.value, function(data) {
+    //$.getJSON('http://nominatim.OpenStreetMap.org/search?format=json&limit=5&q=' + entrada.value, function(data) {
+    $.getJSON('http://nominatim.OpenStreetMap.org/search?format=json&countrycodes=es&limit=5&q=' + entrada.value, function(data) {    
         var direcciones = [];
 
         $.each(data, function(key, val) {
             bb = val.boundingbox;
-            direcciones.push("<li><a href='#' onclick='elegirDireccion(" + bb[0] + ", " + bb[2] + ", " + bb[1] + ", " + bb[3] + ", \"" + val.tipo_osm + "\");return false;'>" + val.display_name + '</a></li>');
+            console.log("### Buscar direccions", val.lat, val.lon);
+            //direcciones.push("<li><a href='#' onclick='elegirDireccion(" + bb[0] + ", " + bb[2] + ", " + bb[1] + ", " + bb[3] + ", \"" + val.tipo_osm + "\");return false;'>" + val.display_name + '</a></li>');
+            direcciones.push("<li><a href='#' onclick='elegirDireccion(" + val.lat + ", " + val.lon  + ", \"" + val.tipo_osm + "\");return false;'>" + val.display_name + '</a></li>');
         });
 
         $('#resultado').empty();
@@ -60,8 +78,6 @@ function direccion_buscador() {
 
     });
 }
-
-
 
 
 function valor(loc1, loc2) {
